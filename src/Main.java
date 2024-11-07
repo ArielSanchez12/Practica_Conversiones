7c76c727d6b5d2dd4da1f3c8450075e51c1b308e
ArielSanchez12 <riveraariel433@gmail.com>
2024-11-07 09:48:43 -0500

    PracticaClase8

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//entero a double
+//entero a cadena
+public class Main {
+    public static void main(String[] args) {
+        int num = 5;
+        String aCadena;
+        double aDouble;
+
+        aCadena = String.valueOf(num);
+        aDouble = (double) num;
+
+        System.out.println("El número entero " + num + " se convierte a cadena: " + aCadena);
+        System.out.println("El número entero " + num + " se convierte a double: " + aDouble);
+        //Ariel Sanchez
+    }
+}
